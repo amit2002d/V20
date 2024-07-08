c0dff9a581dfd264b49085a06a7c9658f6fe2b53
amit2002d <amitgulati78@gmail.com>
2024-07-08 12:26:51 +0530

    message

--- pages/0_SUMMARY.py
+++ pages/0_SUMMARY.py
@@ -155,7 +155,7 @@ while True:
             qty = math.ceil(amount / cmp)
             down_lb = round((cmp - last_buy)/last_buy * 100,2) if last_buy != 0 else 0
             lth = lifetime_high(st.session_state.secrets["connections"]["gsheets"]["worksheets"][stock])
-            if down_lb <= -10:
+            if down_lb <= -14.999:
                 new_res = pd.DataFrame({'Stock': [stock], 'Down%':[round(pnl*100,2)], "Down_LTH%": [round((cmp - lth)/lth * 100,2)], "LTH": [lth], 'Down_LB%':[down_lb],'CMP':[cmp], 'Amount': [amount], 'Qty': [qty], 'LB': [last_buy]})
                 buy = pd.concat([buy,new_res],ignore_index=True)
             elif last_buy == 0 and round((cmp - lth)/lth * 100,2) <= -10:
