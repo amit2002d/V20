b6da4517cb686966c54f42b5d4a03283cecaea6e
amit2002d <amitgulati78@gmail.com>
2024-07-08 12:03:26 +0530

    message

--- pages/0_SUMMARY.py
+++ pages/0_SUMMARY.py
@@ -158,7 +158,7 @@ while True:
             if down_lb <= -10:
                 new_res = pd.DataFrame({'Stock': [stock], 'Down%':[round(pnl*100,2)], "Down_LTH%": [round((cmp - lth)/lth * 100,2)], "LTH": [lth], 'Down_LB%':[down_lb],'CMP':[cmp], 'Amount': [amount], 'Qty': [qty], 'LB': [last_buy]})
                 buy = pd.concat([buy,new_res],ignore_index=True)
-            elif last_buy == 0 and round((cmp - lth)/lth * 100,2) <= -14.9:
+            elif last_buy == 0 and round((cmp - lth)/lth * 100,2) <= -10:
                 new_res = pd.DataFrame({'Stock': [stock], 'Down%':[round(pnl*100,2)], "Down_LTH%": [round((cmp - lth)/lth * 100,2)], "LTH": [lth], 'Down_LB%':[down_lb],'CMP':[cmp], 'Amount': [amount], 'Qty': [qty], 'LB': [last_buy]})
                 buy = pd.concat([buy,new_res],ignore_index=True)
             if buy.empty:
